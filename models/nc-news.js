4777d67f91b05de2d0c598d8c93d0f89fab698c3
bobby.whaler@icloud.com <you@example.com>
2022-12-12 17:21:59 +0000

    changes made after task3 review and task 4 done, except for testing of article date order

--- models/nc-news.js
+++ models/nc-news.js
@@ -5,3 +5,8 @@ exports.selectTopics = () => {
     .query("SELECT * FROM topics;")
     .then(({ rows }) => rows);
 };
+exports.selectArticles = () => {
+    return db
+    .query("SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC;")
+    .then(({ rows }) => rows);
+};
