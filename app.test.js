ed67f43cc2e9f70e20519928bcc35c5f3ba6f423
bobby.whaler@icloud.com <you@example.com>
2022-12-16 15:29:57 +0000

    task 11 update

--- app.test.js
+++ app.test.js
@@ -196,7 +196,7 @@ test("status:200, should return the correct amount of comments for each article"
     });
 });
 
-describe("5/11. GET /api/articles/:article_id", () => {
+describe("5. GET /api/articles/:article_id", () => {
   test("status:200, responds with a single matching article", () => {
     const article_ID = 2;
     return request(app)
@@ -212,7 +212,6 @@ describe("5/11. GET /api/articles/:article_id", () => {
           body: expect.any(String),
           created_at: "2020-10-16T05:03:00.000Z",
           votes: 0,
-          comment_count: '0'
         }));
       });
   });
@@ -645,3 +644,24 @@ describe("10. GET /api/articles (queries)", () => {
   });
 });
 
+describe("11. GET /api/articles/:article_id (comment_count)", () => {
+  test("status:200, responds with a single matching article including comment_count", () => {
+    const article_ID = 2;
+    return request(app)
+      .get(`/api/articles/${article_ID}`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual(
+          expect.objectContaining({
+          article_id: 2,
+          title: "Sony Vaio; or, The Laptop",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: expect.any(String),
+          created_at: "2020-10-16T05:03:00.000Z",
+          votes: 0,
+          comment_count: '0'
+        }));
+      });
+  });
+});
